8aea1f02dc35750ddaf81d7663f206d8069a8aa6
kyohei_i <kyoheishogun.1997@gmail.com>
2021-04-29 08:53:04 +0900

    InitialCommit

---
+++ Kadai4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kadai4
+//
+//  Created by 今村京平 on 2021/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var labal: UILabel!
+    var value = 0
+
+    @IBAction func plusOne(_ sender: Any) {
+        value += 1
+        labal.text = String(value)
+    }
+    @IBAction private func clearBtn(_ sender: Any) {
+        value = 0
+        labal.text = String(value)
+    }
+    
+}
+
